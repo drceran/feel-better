ea6dba2c552876598f489d61a25ba3c5f0ea9621
Esra Ceran <dresraceran@gmail.com>
2023-05-19 00:16:36 +0000

    list all of the users view completed

--- therapy/queries/jotters.py
+++ therapy/queries/jotters.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List, Union
 from enum import Enum
 from queries.pool import pool
 
@@ -7,13 +7,17 @@ from queries.pool import pool
 # from datetime import date
 
 
+class Error(BaseModel):
+    message: str
+
+
 class JotterType(str, Enum):
-    client = 'client'
-    therapist = 'therapist'
+    client = "client"
+    therapist = "therapist"
 
 
 # these are called as typein
-class JottersIn (BaseModel):
+class JottersIn(BaseModel):
     first_name: str
     last_name: str
     email: str
@@ -26,6 +30,7 @@ class JottersIn (BaseModel):
     graduated_college: Optional[str]
     profile_picture: Optional[str]
     about_me: Optional[str]
+
 
 class JottersOut(BaseModel):
     id: int
@@ -44,6 +49,53 @@ class JottersOut(BaseModel):
 
 
 class JottersRepository:
+    def get_all_jotters(self) -> Union[Error, List[JottersOut]]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT
+                        id,
+                        first_name,
+                        last_name,
+                        email,
+                        type,
+                        phone_number,
+                        city,
+                        state,
+                        balance,
+                        certificates,
+                        graduated_college,
+                        profile_picture,
+                        about_me
+                        FROM jotters
+                        ORDER BY id;
+                        """
+                    )
+                    result = []
+                    for record in db:
+                        jotter = JottersOut(
+                            id=record[0],
+                            first_name=record[1],
+                            last_name=record[2],
+                            email=record[3],
+                            type=record[4],
+                            phone_number=record[5],
+                            city=record[6],
+                            state=record[7],
+                            balance=record[8],
+                            certificates=record[9],
+                            graduated_college=record[10],
+                            profile_picture=record[11],
+                            about_me=record[12],
+                        )
+                        result.append(jotter)
+                    return result
+        except Exception as e:
+            print(e)
+            return {"message": "Could not get all jotters"}
+
     def create(self, jotter: JottersIn) -> JottersOut:
         with pool.connection() as conn:
             with conn.cursor() as db:
