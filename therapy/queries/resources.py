78e45175fee34ecaac463837f3b0c25a3e22f40f
Melody <melodyoberg@icloud.com>
2023-05-22 20:30:09 +0000

    Resources backend endpoints

--- therapy/queries/resources.py
+++ therapy/queries/resources.py
@@ -1,35 +1,132 @@
 from pydantic import BaseModel
-from typing import Optional
-from datetime import date
+from typing import Optional, List, Union
+from datetime import datetime
 from queries.pool import pool
+
+class Error(BaseModel):
+    message: str
 
 class ResourceIn(BaseModel):
     title: str
     body: str
     writer: str
-    posted_date: date
     picture: Optional[str]
     url_link: Optional[str]
 
 
 class ResourceOut(ResourceIn):
     id: int
+    posted_date: datetime = datetime.now()
 
 
 class ResourceRepository:
-    def create(self, resource: ResourceIn):
-        with pool.connection() as conn:
-            with conn.cursor() as db:
-                result = db.execute(
-                    """
-                    INSERT INTO resources
-                        (title, body, writer, posted_date, picture, url_link)
-                    VALUES
-                        (%s, %s, %s, %s, %s, %s)
-                    RETURNING id;
-                    """,
-                    [resource.title, resource.body, resource.writer, resource.posted_date, resource.picture, resource.url_link]
-                )
-                id = result.fetchone()[0]
-                old = resource.dict()
-                return ResourceOut(id=id, **old)
+    def get_resource(self, resource_id: int) -> Union[Error, ResourceOut]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT * FROM resources
+                        WHERE id = %s
+                        """,
+                        [resource_id,]
+                    )
+                    record = result.fetchone()
+                if record:
+                    return self.record_to_resource_out(record)
+                else:
+                    return {"message": "Resource does not exist."}
+        except Exception as e:
+            return {"message": "Cannot load resource details."}
+
+    def delete_resource(self, resource_id: int) -> bool:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        DELETE FROM resources
+                        WHERE id = %s
+                        """,
+                        [resource_id,]
+                    )
+                    return True
+        except Exception:
+            return False
+
+    def update(self, resource_id: int, resource:ResourceIn) -> Union[ResourceOut, Error]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        UPDATE resources
+                        SET title = %s, body = %s, writer = %s, picture = %s, url_link = %s
+                        WHERE id = %s
+                        """,
+                        [
+                            resource.title,
+                            resource.body,
+                            resource.writer,
+                            resource.picture,
+                            resource.url_link,
+                            resource_id,
+
+                        ]
+                    )
+                return self.resource_in_to_out(resource_id, resource)
+        except Exception:
+            return {"message": "Could not update resource."}
+
+    def get_all_resources(self) -> Union[Error, List[ResourceOut]]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT id, title, body, writer, picture, url_link, posted_date
+                        FROM resources
+                        ORDER BY posted_date;
+                        """
+                    )
+                    return [self.record_to_resource_out(record) for record in result]
+        except Exception:
+            return {"message": "Could not get resources."}
+
+    def create(self, resource: ResourceIn) -> ResourceOut:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        INSERT INTO resources
+                            (title, body, writer, picture, url_link)
+                        VALUES
+                            (%s, %s, %s, %s, %s)
+                        RETURNING id, posted_date;
+                        """,
+                        [resource.title, resource.body, resource.writer, resource.picture, resource.url_link]
+                    )
+                    id, posted_date = result.fetchone()
+                    old = resource.dict()
+                return ResourceOut(id=id, posted_date=posted_date, **old)
+        except Exception:
+            return {"message": "Could not create resource."}
+
+    def resource_in_to_out(self, id:int, resource: ResourceIn):
+        old_data = resource.dict()
+        return ResourceOut(id=id, **old_data)
+
+    def record_to_resource_out(self, record):
+        if record and len(record) >= 7:
+            return ResourceOut(
+                id=record[0],
+                title=record[1],
+                body=record[2],
+                writer=record[3],
+                picture=record[4],
+                url_link=record[5],
+                posted_date=record[6]
+            )
+        else:
+            return None
--- therapy/routers/resources.py
+++ therapy/routers/resources.py
@@ -1,9 +1,30 @@
-from fastapi import APIRouter, Depends
-from queries.resources import ResourceIn, ResourceRepository
+from fastapi import APIRouter, Depends, Response
+from typing import Union, List, Optional #union means more than one thing
+from datetime import datetime
+from queries.resources import ResourceIn, ResourceRepository, ResourceOut, Error
 
 router = APIRouter()
 
 
-@router.post("/resources")
-def create_resource(resource: ResourceIn, repo: ResourceRepository = Depends()):
+@router.post("/resources", response_model=Union[ResourceOut, Error])
+def create_resource(resource: ResourceIn, response: Response, repo: ResourceRepository = Depends()):
     return repo.create(resource)
+
+@router.get("/resources", response_model=Union[Error, List[ResourceOut]])
+def get_all_resources(
+    repo: ResourceRepository = Depends(),
+):
+    return repo.get_all_resources()
+
+@router.get("/resources/{resource_id}", response_model=Optional[ResourceOut])
+def get_resource(resource_id: int, repo: ResourceRepository = Depends()) ->bool:
+        return repo.get_resource(resource_id)
+
+@router.delete("/resources/{resource_id}", response_model=bool)
+def delete_resource(resource_id: int, repo: ResourceRepository = Depends()) ->bool:
+        return repo.delete_resource(resource_id)
+
+@router.put("/resources/{resource_id}", response_model=Union[Error, ResourceOut])
+def update_resource(resource_id: int, resource: ResourceIn, repo: ResourceRepository = Depends()
+                    ) -> Union[Error, ResourceOut]:
+                    return repo.update(resource_id, resource)
