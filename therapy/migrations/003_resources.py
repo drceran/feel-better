4c99cada1f7b4a635dae6f43c2c7d09aeb5cebca
Melody <melodyoberg@icloud.com>
2023-05-23 16:02:31 -0700

    journal and edited resource table

--- therapy/migrations/003_resources.py
+++ therapy/migrations/003_resources.py
@@ -1,18 +1,22 @@
 steps = [
     [
-        # "Up" SQL statement
         """
+        CREATE TYPE user_type AS ENUM ('client', 'therapist');
         CREATE TABLE resources (
             id SERIAL PRIMARY KEY NOT NULL,
             title VARCHAR(200) NOT NULL,
             body VARCHAR(1000) NOT NULL,
-            writer VARCHAR(200) NOT NULL,
+            writer INTEGER REFERENCES jotters(id),
             picture VARCHAR(500) NULL,
             url_link VARCHAR(500) NULL,
-            posted_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
-        );
+            posted_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+            CONSTRAINT therapist_writer CHECK (
+                writer IN (
+                    SELECT id FROM jotters WHERE user_type = 'therapist'
+                )
+        )
+);
         """,
-        # "Down" SQL statement
         """
         DROP TABLE resources;
         """,
