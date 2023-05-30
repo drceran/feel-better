e993ce7a4895ff0467228526e63cb3b05e35324b
Daniel Ahdoot <zipster000@yahoo.com>
2023-05-30 09:40:22 -0700

    before i break it

--- ghi/src/Pricing.js
+++ ghi/src/Pricing.js
@@ -21,7 +21,7 @@ export default function Pricing() {
                     <div className="p-8 sm:p-10 lg:flex-auto">
                         <h3 className="text-2xl font-bold tracking-tight text-gray-900">Monthly membership</h3>
                         <p className="mt-6 text-base leading-7 text-gray-600">
-                            Get access to all of our resources and therapists for one low monthly price. No hidden fees
+                            Get access to all of our resources and therapists for one low monthly price. No hidden fees.
                         </p>
                         <div className="mt-10 flex items-center gap-x-4">
                             <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">What's included</h4>
