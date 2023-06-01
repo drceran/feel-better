a3c1fa999d73c76a59bddd7cc9f6d23ec607b296
David Plaskett <mrtinguy@gmail.com>
2023-06-01 10:10:15 -0600

    correcting a bad commit

--- therapy/main.py
+++ therapy/main.py
@@ -24,6 +24,15 @@ app.include_router(accounts.router)
 app.include_router(chat.router)
 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.environ.get("CORS_HOST", "http://localhost:3000")],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/")
 def launch_details():
     return {
