43dde87b161a49fa204bc390c6bb0d18c84a2ef8
David Plaskett <mrtinguy@gmail.com>
2023-06-01 08:45:41 -0600

    Updated the authSlice to properly handle the token for different pages and updated journal/getall to pull only the needed journals from the database

--- therapy/main.py
+++ therapy/main.py
@@ -14,14 +14,6 @@ import os
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[os.environ.get("CORS_HOST", "http://localhost:3000")],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 app.include_router(appointments.router)
 app.include_router(jotters.router)
 app.include_router(resources.router)
