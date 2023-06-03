f8751b7b8a70777b46de9b535d31e8704a20d784
Esra Ceran <dresraceran@gmail.com>
2023-06-03 01:16:01 +0000

    Esra2

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -127,6 +127,22 @@ export default function Nav() {
                             {({ active }) => (
                               <a
                                 href="#"
+                                onClick={() =>
+                                  navigate("/editProfile/" + data.account.id)
+                                }
+                                className={classNames(
+                                  active ? "bg-gray-100" : "",
+                                  "block px-4 py-2 text-sm text-gray-700"
+                                )}
+                              >
+                                Edit Profile
+                              </a>
+                            )}
+                          </Menu.Item>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="#"
                                 onClick={() => navigate("/logout")}
                                 className={classNames(
                                   active ? "bg-gray-100" : "",
