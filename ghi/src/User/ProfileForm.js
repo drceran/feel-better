b1bc15e5b97438b50951624b4fd8c3d3f11dcf5a
Esra Ceran <dresraceran@gmail.com>
2023-06-02 17:50:35 -0700

    deleted unused files

--- ghi/src/User/ProfileForm.js
+++ ghi/src/User/ProfileForm.js
@@ -126,10 +126,12 @@ function ProfileForm() {
             </div>
             <div>
               <button
-                className="btn btn-primary"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                 type="submit"
                 value="Save Changes"
-              />
+              >
+                Save Changes
+              </button>
             </div>
           </form>
         </div>
