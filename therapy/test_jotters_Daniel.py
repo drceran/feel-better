13b0587d4121d2a34b5786256e71c203785053b2
Daniel Ahdoot <zipster000@yahoo.com>
2023-06-06 16:41:05 -0700

    made changes for lint

--- therapy/test_jotters_Daniel.py
+++ therapy/test_jotters_Daniel.py
@@ -3,7 +3,6 @@ from main import app
 from typing import Optional
 from pydantic import BaseModel
 from authenticator import authenticator
-from queries.jotters import JottersRepository
 
 client = TestClient(app)
 
@@ -74,8 +73,7 @@ def fake_jotters_in(jotter):
     )
 
 def test_get_all_jotters():
-    app.dependency_overrides[authenticator.get_current_account_data
-        ] = fake_get_current_jotters_data
+    app.dependency_overrides[authenticator.get_current_account_data] = fake_get_current_jotters_data
     response = client.get("/jotters")
     app.dependency_overrides = {}
     assert response.status_code == 200
@@ -83,135 +81,8 @@ def test_get_all_jotters():
 
 
 def test_get_one_jotters():
-    app.dependency_overrides[authenticator.get_current_account_data
-        ] = fake_get_current_jotters_data
+    app.dependency_overrides[authenticator.get_current_account_data] = fake_get_current_jotters_data
     response = client.get("/jotters/1")
     app.dependency_overrides = {}
     assert response.status_code == 200
     assert isinstance(response.json(), dict)
-
-
-
-# class GetOneJotter:
-#     def get_one_jotter(self, jotter_id):
-#         jotter_id = 1
-#         return {
-#             "id": jotter_id,
-#             "first_name": "Test",
-#             "last_name": "Jotter",
-#             "email": "a@a.com",
-#             "type": "client",
-#             "phone_number": "1234567890",
-#             "city": "LA",
-#             "state": "CA",
-#             "balance": 0,
-#             "certification": "Test",
-#             "graduated_college": "Test",
-#             "profile_picture": "Test",
-#             "about_me": "Test",
-#             "password": "password123",
-#         }
-
-# class UpdateJotter:
-#     def update_jotter(self, jotter_id, jotter):
-#         jotter_id = 1
-#         return {
-#             "id": jotter_id,
-#             **jotter.dict(),
-#         }
-
-# class CreateJotter:
-#     def create_jotter(self, jotter):
-#         return {
-#             "id": 1,
-#             **jotter.dict(),
-#         }
-
-# def test_create_jotter():
-#     app.dependency_overrides[JottersRepository] = CreateJotter
-
-#     json = {
-#         "first_name": "Test",
-#         "last_name": "Jotter",
-#         "email": "a@a.com",
-#         "type": "client",
-#         "phone_number": "1234567890",
-#         "city": "LA",
-#         "state": "CA",
-#         "balance": 0,
-#         "certification": "Test",
-#         "graduated_college": "Test",
-#         "profile_picture": "Test",
-#         "about_me": "Test",
-#         "password": "password123",
-#     }
-
-#     expected = {
-#         "id": 1,
-#         **json,
-#     }
-
-#     response = client.post("/jotters", json=json, headers={"Authorization": "{token}"})
-#     app.dependency_overrides = {}
-#     assert response.status_code == 200
-#     assert response.json() == expected
-
-
-# def test_get_one_jotter():
-#     app.dependency_overrides[JottersRepository] = GetOneJotter
-
-#     json = {
-#         "id": 1,
-#         "first_name": "Test",
-#         "last_name": "Jotter",
-#         "email": "a@a.com",
-#         "type": "client",
-#         "phone_number": "1234567890",
-#         "city": "LA",
-#         "state": "CA",
-#         "balance": 0,
-#         "certification": "Test",
-#         "graduated_college": "Test",
-#         "profile_picture": "Test",
-#         "about_me": "Test",
-#         "password": "password123",
-#     }
-
-#     expected = {
-#         "id": 1,
-#         **json,
-#     }
-
-#     response = client.get("/jotters/1")
-#     app.dependency_overrides = {}
-#     assert response.status_code == 200
-#     assert response.json() == expected
-
-
-# def test_update_jotter():
-#     app.dependency_overrides[JottersRepository] = UpdateJotter
-
-#     json = {
-#         "first_name": "Updated",
-#         "last_name": "Jotter",
-#         "email": "b@b.com",
-#         "type": "client",
-#         "phone_number": "1234567890",
-#         "city": "LA",
-#         "state": "CA",
-#         "balance": 0,
-#         "certification": "Test",
-#         "graduated_college": "Test",
-#         "profile_picture": "Test",
-#         "about_me": "Test",
-#         "password": "password123",
-#     }
-#     expected = {
-#         "id": 1,
-#         **json,
-#     }
-
-#     response = client.put("/jotters/1", json=json)
-#     app.dependency_overrides = {}
-#     assert response.status_code == 200
-#     assert response.json() == expected
