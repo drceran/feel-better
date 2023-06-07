e5eb68729d0fff9066ab92b08ea75b223def5278
Daniel Ahdoot <zipster000@yahoo.com>
2023-06-07 15:50:17 -0700

    I HATE GIT

--- ghi/src/WebPlayback.jsx
+++ ghi/src/WebPlayback.jsx
@@ -1,29 +1,29 @@
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
-// function WebPlayback(props) {
-//   const [token, setToken] = useState("");
+function WebPlayback(props) {
+  const [token, setToken] = useState("");
 
-//   useEffect(() => {
-//     window.onSpotifyWebPlaybackSDKReady = () => {
-//       const player = new Spotify.Player({
-//         name: "Journal Jotters",
-//         getOAuthToken: (callback) => {
-//           callback(token);
-//         },
-//       });
+  useEffect(() => {
+    window.onSpotifyWebPlaybackSDKReady = () => {
+      const player = new Spotify.Player({
+        name: "Journal Jotters",
+        getOAuthToken: (callback) => {
+          callback(token);
+        },
+      });
 
-//       player.connect();
-//     };
-//   }, [token]);
+      player.connect();
+    };
+  }, [token]);
 
-//   return (
-//     <>
-//       <div className="container">
-//         <div className="main-wrapper">
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <div className="container">
+        <div className="main-wrapper">
+        </div>
+      </div>
+    </>
+  );
+}
 
-// export default WebPlayback;
+export default WebPlayback;
