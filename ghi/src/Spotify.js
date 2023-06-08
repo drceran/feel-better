d08dae1f28d2b0a4fd38e6d8e79d32e8081029ce
Daniel Ahdoot <zipster000@yahoo.com>
2023-06-08 15:33:00 -0700

    before i change spotify and break it

--- ghi/src/Spotify.js
+++ ghi/src/Spotify.js
@@ -7,37 +7,57 @@ import SpotifyPlayer from 'react-spotify-player';
 
 const Spotify = () => {
     const spotify = Credentials();
+    const CLIENT_ID = "c7543eda9c9b4e4a8fd6bcdb7f7d7f41";
+    const REDIRECT_URI = "http://localhost:3000/spotify";
+    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+    const RESPONSE_TYPE = "token";
+
 
     const [token, setToken] = useState('');
+    const [loggedIn, setLoggedIn] = useState(false);
     const [genres, setGenres] = useState({ selectedGenre: '', listOfGenresFromAPI: [] });
     const [playlist, setPlaylist] = useState({ selectedPlaylist: '', listOfPlaylistFromAPI: [] });
     const [tracks, setTracks] = useState({ selectedTrack: '', listOfTracksFromAPI: [] });
     const [trackDetail, setTrackDetail] = useState(null);
 
     useEffect(() => {
-        axios('https://accounts.spotify.com/api/token', {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': 'Basic ' + btoa(spotify.ClientId + ':' + spotify.ClientSecret)
-            },
-            data: 'grant_type=client_credentials',
-            method: 'POST'
-        })
-            .then(tokenResponse => {
-                setToken(tokenResponse.data.access_token);
+        const accessToken = localStorage.getItem('accessToken');
+        if (accessToken) {
+            setToken(accessToken);
+            setLoggedIn(true);
+        }
+    }, []);
 
-                axios('https://api.spotify.com/v1/browse/categories?locale=sv_US', {
-                    method: 'GET',
-                    headers: { 'Authorization': 'Bearer ' + tokenResponse.data.access_token }
-                })
-                    .then(genreResponse => {
-                        setGenres({
-                            selectedGenre: genres.selectedGenre,
-                            listOfGenresFromAPI: genreResponse.data.categories.items
-                        })
+    const authenticate = () => {
+        window.location = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=${RESPONSE_TYPE}`;
+    };
+
+    useEffect(() => {
+        if (window.location.hash) {
+            const params = window.location.hash.substring(1).split('&');
+            const accessToken = params.find(param => param.includes('access_token='));
+            if (accessToken) {
+                setToken(accessToken.split('=')[1]);
+                setLoggedIn(true);
+                localStorage.setItem('accessToken', accessToken.split('=')[1]);
+            }
+        }
+    }, []);
+
+    useEffect(() => {
+        if (token) {
+            axios(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
+                method: 'GET',
+                headers: { 'Authorization': 'Bearer ' + token }
+            })
+                .then(genreResponse => {
+                    setGenres({
+                        selectedGenre: genres.selectedGenre,
+                        listOfGenresFromAPI: genreResponse.data.categories.items
                     });
-            });
-    }, [genres.selectedGenre, spotify.ClientId, spotify.ClientSecret]);
+                });
+        }
+    }, [token]);
 
     const genreChanged = val => {
         setGenres({
@@ -87,34 +107,57 @@ const Spotify = () => {
         setTrackDetail(trackInfo[0].track);
     };
 
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6">
-                    <form onSubmit={buttonClicked}>
-                        <Dropdown label="Genre :" options={genres.listOfGenresFromAPI} selectedValue={genres.selectedGenre} changed={genreChanged} />
-                        <Dropdown label="Playlist :" options={playlist.listOfPlaylistFromAPI} selectedValue={playlist.selectedPlaylist} changed={playlistChanged} />
-                        <div className="col-sm-6 row form-group px-0">
-                            <button type='submit' className="btn btn-success col-sm-12">
-                                Search
-                            </button>
-                        </div>
-                    </form>
-                    <Listbox items={tracks.listOfTracksFromAPI} clicked={listboxClicked} />
-                </div>
-                {trackDetail && (
-                    <div className="col-md-6">
-                        <SpotifyPlayer
-                            uri={trackDetail.uri}
-                            size="large"
-                            view="coverart"
-                            theme="black"
-                        />
-                    </div>
-                )}
+    const logout = () => {
+        localStorage.removeItem('accessToken');
+        setToken('');
+        setLoggedIn(false);
+    };
+
+    const renderLoginView = () => {
+        return (
+            <div className="container">
+                <button onClick={authenticate}>Log in with Spotify</button>
             </div>
-        </div>
-    );
+        );
+    };
+
+    const renderMainView = () => {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <form onSubmit={buttonClicked}>
+                            <Dropdown label="Genre :" options={genres.listOfGenresFromAPI} selectedValue={genres.selectedGenre} changed={genreChanged} />
+                            <Dropdown label="Playlist :" options={playlist.listOfPlaylistFromAPI} selectedValue={playlist.selectedPlaylist} changed={playlistChanged} />
+                            <div className="col-sm-6 row form-group px-0">
+                                <button type='submit' className="btn btn-success col-sm-12">
+                                    Search
+                                </button>
+                            </div>
+                        </form>
+                        <Listbox items={tracks.listOfTracksFromAPI} clicked={listboxClicked} />
+                    </div>
+                    {trackDetail && (
+                        <div className="col-md-6">
+                            <SpotifyPlayer
+                                uri={trackDetail.uri}
+                                size="large"
+                                view="coverart"
+                                theme="black"
+                            />
+                        </div>
+                    )}
+                </div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <button onClick={logout}>Log out</button>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+
+    return loggedIn ? renderMainView() : renderLoginView();
 };
 
 export default Spotify;
