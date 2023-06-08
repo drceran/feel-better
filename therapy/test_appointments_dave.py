6da212eb2bfd9f7fd12a0708c4d24777e933f911
Daniel Ahdoot <zipster000@yahoo.com>
2023-06-08 10:02:05 -0700

    wow

--- therapy/test_appointments_dave.py
+++ therapy/test_appointments_dave.py
@@ -1,70 +1,46 @@
 from fastapi.testclient import TestClient
 from main import app
-from queries.appointments import AppointmentRepository
+from queries.messages import MessageRepository
 from pydantic import BaseModel
-from typing import Optional
+from authenticator import authenticator
+from datetime import datetime
+
 
 client = TestClient(app)
 
 
-class AppointmentsOut(BaseModel):
+class MessageOut(BaseModel):
     id: int
-    jotter_id: int
-    client_id: int
-    date: str
-    time: str
-    duration: int
-    price: int
-    status: str
-    jotter_first_name: str
-    jotter_last_name: str
-    jotter_email: str
-    jotter_type: str
-    jotter_phone_number: Optional[str] = None
-    jotter_city: Optional[str] = None
-    jotter_state: Optional[str] = None
-    jotter_balance: Optional[int] = None
-    jotter_certification: Optional[str]
-    jotter_graduated_college: Optional[str]
-    jotter_profile_picture: Optional[str]
-    jotter_about_me: Optional[str]
-    jotter_password: Optional[str]
-    client_first_name: str
-    client_last_name: str
-    client_email: str
-    client_type: str
-    client_phone_number: Optional[str] = None
-    client_city: Optional[str] = None
-    client_state: Optional[str] = None
-    client_balance: Optional[int] = None
-    client_certification: Optional[str]
-    client_graduated_college: Optional[str]
-    client_profile_picture: Optional[str]
-    client_about_me: Optional[str]
-    client_password: Optional[str]
+    user_id: int
+    recipient: int
+    subject: str
+    body: str
+    datetime: datetime
 
 
-class EmptyAppointmentRepo:
-    def get_all_appointments(self, id=None):
+class emptyRepo:
+    def get_all_messages(self, id=None):
         return []
 
 
-def fake_appointment_data():
-    appointment = AppointmentsOut(
+def fake_message_data():
+    message = MessageOut(
         id=1,
-        jotter_id=1,
-        client_id=1,
-        date="2021-09-01",
-        time="12:00",
-        duration=1,
-        price=1,
+        user_id=1,
+        recipient=2,
+        subject="Hello",
+        body="goodbye!",
+        datetime=datetime.now(),
     )
-    return appointment.__dict__
+    return message.__dict__
 
 
-def test_get_appointments():
-    app.dependency_overrides[AppointmentRepository] = EmptyAppointmentRepo
-    response = client.get("/appointments/1")
+def test_get_all_messages():
+    app.dependency_overrides[MessageRepository] = emptyRepo
+    app.dependency_overrides[
+        authenticator.get_current_account_data
+    ] = fake_message_data
+    response = client.get("/messages")
     app.dependency_overrides = {}
     assert response.status_code == 200
-    assert isinstance(response.json(), dict)
+    assert response.json() == []
--- therapy/test_jotter_Esra.py
+++ therapy/test_jotter_Esra.py
@@ -1,57 +1,46 @@
 from fastapi.testclient import TestClient
 from main import app
-from queries.jotters import JottersRepository
+from queries.appointments import AppointmentRepository
 from pydantic import BaseModel
-from typing import Optional
+from authenticator import authenticator
+from datetime import date, time
+
 
 client = TestClient(app)
 
-
-class JottersOut(BaseModel):
+class AppointmentOut(BaseModel):
     id: int
-    first_name: str
-    last_name: str
-    email: str
-    type: str
-    phone_number: Optional[str] = None
-    city: Optional[str] = None
-    state: Optional[str] = None
-    balance: Optional[int] = None
-    certification: Optional[str]
-    graduated_college: Optional[str]
-    profile_picture: Optional[str]
-    about_me: Optional[str]
-    password: Optional[str]
+    user_id: int
+    therapist_id: int
+    appointment_date: date
+    appointment_time: time
+    cost: float
 
-
-class EmptyJotterRepo:
-    def get_all_jotters(self, id=None):
+class EmptyRepo:
+    def get_all_appointments(self, id=None):
         return []
 
+    def get_all_appointments_for_user(self, user_id):
+        return []
 
-def fake_jotters_account_data():
-    jotter = JottersOut(
+def fake_appointment_data():
+    appointment = AppointmentOut(
         id=1,
-        first_name="Test",
-        last_name="Account",
-        email="a@a.com",
-        type="client",
-        phone_number="1234567890",
-        city="LA",
-        state="CA",
-        balance=0,
-        certification="Test",
-        graduated_college="Test",
-        profile_picture="Test",
-        about_me="Test",
-        password="password123",
+        user_id=1,
+        therapist_id=1,
+        appointment_date=date.today(),
+        appointment_time=time(12, 30),
+        cost=100.00,
     )
-    return jotter.__dict__
+    return appointment.__dict__
 
 
-def test_get_one_jotters():
-    app.dependency_overrides[JottersRepository] = EmptyJotterRepo
-    response = client.get("/jotters/1")
+def test_get_all_appointments():
+    app.dependency_overrides[AppointmentRepository] = EmptyRepo
+    app.dependency_overrides[
+        authenticator.get_current_account_data
+    ] = fake_appointment_data
+    response = client.get("/appointments")
     app.dependency_overrides = {}
     assert response.status_code == 200
     assert response.json() == []
