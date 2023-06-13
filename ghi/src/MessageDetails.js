ba7a36effdbd4f63da0335daacd00fe2f53f770f
Daniel Ahdoot <zipster000@yahoo.com>
2023-06-13 12:04:46 -0700

    tailwind for detail messages page

--- ghi/src/MessageDetails.js
+++ ghi/src/MessageDetails.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { useGetOneMessageQuery } from './store/messagesAPI';
-
+// import { useGetUserInfoQuery } from './store/usersApi';
 
 function MessageDetails() {
   const { id } = useParams();
   const { data: message, error, isLoading } = useGetOneMessageQuery(id);
+  // const { data: userInfo } = useGetUserInfoQuery();
 
   if (isLoading) {
     return <h1>Loading page...</h1>;
@@ -22,12 +23,49 @@ function MessageDetails() {
         <Link to="/messages">Back to Messages</Link> {' '}
         <Link to={`/messages/${id}/edit`}>Edit</Link>
       </div>
-      <h2>{message.subject}</h2>
-      <p>Body: {message.body}</p>
-      <p>Recipient: {message.recipient}</p>
-      <p>Date and Time: {new Date(message.datetime).toLocaleString()}</p>
+      <div className="px-4 sm:px-0">
+        <h3 className="text-base font-semibold leading-7 text-gray-900">Message Information</h3>
+        <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Personal details and application.</p>
+      </div>
+      <div className="mt-6 border-t border-gray-100">
+        <dl className="divide-y divide-gray-100">
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Sender name</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{message.user_id}</dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Subject</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{message.subject}</dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Time sent</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{message.datetime}</dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Message</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {message.body}
+            </dd>
+          </div>
+        </dl>
+      </div>
     </div>
   );
 }
+
+
+    // <div>
+    //   <div style={{ marginBottom: '10px' }}>
+    //     <Link to="/home">Home</Link> {' '}
+    //     <Link to="/messages">Back to Messages</Link> {' '}
+    //     <Link to={`/messages/${id}/edit`}>Edit</Link>
+    //   </div>
+//       <h2>{message.subject}</h2>
+//       <p>Body: {message.body}</p>
+//       <p>Recipient: {message.recipient}</p>
+//       <p>Date and Time: {new Date(message.datetime).toLocaleString()}</p>
+//     </div>
+//   );
+// }
 
 export default MessageDetails;
