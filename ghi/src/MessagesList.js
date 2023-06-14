d8c7f6cae29d892136e8611975de96e103312953
Daniel Ahdoot <zipster000@yahoo.com>
2023-06-14 14:02:59 -0700

    working

--- ghi/src/MessagesList.js
+++ ghi/src/MessagesList.js
@@ -3,12 +3,14 @@ import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { useGetMessagesQuery, useDeleteMessageMutation } from './store/messagesAPI';
 import { selectMessage } from './store/messagesSlice';
+import { useGetClientDetailQuery } from './store/usersApi';
 
 function MessagesList() {
     const { data: messages, error, isLoading } = useGetMessagesQuery();
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const [deleteMessage] = useDeleteMessageMutation();
+    const { data: userInfo } = useGetClientDetailQuery();
 
     useEffect(() => {
         if (deleteMessage.isSuccess) {
@@ -54,6 +56,7 @@ function MessagesList() {
     return (
         <div className="mx-auto w-full max-w-5xl bg-white">
             <ul className="flex flex-col">
+                <button onClick={() => handleGoToAnotherPage(messages)} className="bg-sheer hover:bg-sheer text-billow font-bold py-1 px-4 rounded" style={{ backgroundColor: '#BEC6C3', color: '#626670' }}>Create a New Message</button>
                 {recentMessages.map(messages => (
                     <li key={messages.id} className="border-b-2 border-gray-100">
                         <div className={`py-5 px-4 flex justify-between border-l-4 border-transparent bg-transparent ${messages.online ? "hover:border-green-400 hover:bg-gray-200" : "hover:border-red-500 hover:bg-red-50"}`}>
@@ -61,11 +64,11 @@ function MessagesList() {
                             {/* USER DETAILS */}
                             <div className="sm:pl-4 pr-8 flex sm:items-center">
                                 {/* User Picture */}
-                                <img src={messages.picture} alt="Profile" className="mr-3 w-8 sm:w-12 h-8 sm:h-12 rounded-full" />
+                                <img src={messages.profile_picture} alt="Profile" className="mr-3 w-8 sm:w-12 h-8 sm:h-12 rounded-full" />
                                 {/* User Infos */}
                                 <div className="space-y-1">
                                     {/* Name */}
-                                    <p className="text-base text-gray-700 font-bold tracking-wide">{messages.user_id}</p>
+                                    <p className="text-base text-gray-700 font-bold tracking-wide">{userInfo?.first_name}</p>
                                     {/* Role */}
                                     <p className="text-sm text-gray-500 font-medium">{messages.subject}</p>
                                     <p className="text-sm text-gray-500 font-medium">{messages.body}</p>
@@ -82,7 +85,6 @@ function MessagesList() {
                                 >
                                     Details
                                 </Link>
-                                <button onClick={() => handleGoToAnotherPage(messages)} className="text-sm text-gray-500 font-semibold hover:underline hover:text-gray-700">New Message</button>
                                 <button onClick={() => handleDelete(messages.id)} className="text-sm text-gray-500 font-semibold hover:underline hover:text-gray-700">Delete</button>
                             </div>
                         </div>
